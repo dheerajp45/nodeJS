638d3eb6d804af5fc4e9df2d2563fcdd897296a9
Dheeraj Panyam <dheeraj.panyam@gmail.com>
2026-02-18 22:54:02 +0530

    jwt adeded

--- 08_url_shortner/service/auth.js
+++ 08_url_shortner/service/auth.js
@@ -1,10 +1,22 @@
-const sessionIdToUsermap = new Map();
-function setUser(id,user){
-    sessionIdToUsermap.set(id,user);
+// const sessionIdToUsermap = new Map();
+const jwt = require('jsonwebtoken');
+const secret="dheerajp45"
+
+function setUser(user){
+    user ={
+        _id : user._id,
+        email:user.email,
+    }
+    return jwt.sign(user,secret)
 }
 
-function  getUser(id){
-    return sessionIdToUsermap.get(id);
+function  getUser(token){
+    if(!token) return null;
+    try {
+        return jwt.verify(token,secret);
+    } catch (error) {
+        return null;
+    }
 }
 
 module.exports = {
